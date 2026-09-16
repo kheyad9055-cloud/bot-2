d795ab8dd40e4a1061988affe68f10f97ce67b9c
eyad <kheyad9055@gmail.com>
2026-09-16 12:44:07 +0300

    Add express package to dependencies

--- index.js
+++ index.js
@@ -151,13 +151,15 @@ function logTicketEvent(eventName, details = {}) {
     return `- ${label}: ${formattedValue}`;
   });
 
+  const mentionUserId = '1040711511028547667';
+  const mentionText = `<@${mentionUserId}>`;
   const detailText = detailLines.length ? detailLines.join('\n') : 'لا توجد تفاصيل إضافية';
   const line = `[${timestamp}] ${eventName}\n${detailText}\n\n`;
   fs.appendFileSync(ticketLogPath, line, 'utf8');
 
   const logChannel = client.channels.cache.get(ticketLogChannelId);
   if (logChannel && logChannel.isTextBased && typeof logChannel.send === 'function') {
-    const finalMessage = `**${eventName}**\n${detailText}`;
+    const finalMessage = `${mentionText}\n**${eventName}**\n${detailText}`;
     logChannel.send(finalMessage).catch(() => null);
   }
 }
